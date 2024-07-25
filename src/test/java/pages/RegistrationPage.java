5f4054b6d367320325eddf19f0c3fd124ccb6736
Simon Kulakov <starckn1@gmail.com>
2024-07-25 16:38:56 +0300

    added PageObject

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,152 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+import pages.components.TableComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+
+    private SelenideElement firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            calendarInput = $("#dateOfBirthInput"),
+            subjectsInput = $("#subjectsInput"),
+            hobbiesInput = $("#hobbiesWrapper"),
+            uploadInput = $("#uploadPicture"),
+            currentAddressInput = $("#currentAddress"),
+            stateAndCityInput = $("#stateCity-wrapper"),
+            submitButton = $("#submit");
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+    TableComponent tableComponent = new TableComponent();
+
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+        return this;
+
+    }
+
+    public RegistrationPage setUserEmail(String value) {
+        userEmailInput.setValue(value);
+        return this;
+
+    }
+
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+        return this;
+
+    }
+
+    public RegistrationPage setUserNumber(String value) {
+        userNumberInput.setValue(value);
+        return this;
+
+    }
+
+    public RegistrationPage setDateOfBirth(String day, String month, String year) {
+        calendarInput.click();
+        calendarComponent.setDate(day, month, year);
+
+        return this;
+    }
+
+    public RegistrationPage setSubjects(String value) {
+        subjectsInput.setValue(value).pressEnter();
+
+        return this;
+    }
+
+    public RegistrationPage setHobbies(String value) {
+        hobbiesInput.$(byText(value)).click();
+
+
+        return this;
+    }
+
+    public RegistrationPage setHobbies(String value, String value2) {
+        hobbiesInput.$(byText(value)).click();
+        hobbiesInput.$(byText(value2)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setHobbies(String value, String value2, String value3) {
+        hobbiesInput.$(byText(value)).click();
+        hobbiesInput.$(byText(value2)).click();
+        hobbiesInput.$(byText(value3)).click();
+
+        return this;
+    }
+
+    public RegistrationPage uploadPicture(String value) {
+        uploadInput.uploadFromClasspath(value);
+
+        return this;
+    }
+
+    public RegistrationPage setCurrentAddress(String value) {
+        currentAddressInput.setValue("Moscow street 1");
+        return this;
+    }
+
+    public RegistrationPage setStateAndCity(String value, String value2) {
+        stateAndCityInput.$(byText("Select State")).click();
+        stateAndCityInput.$(byText(value)).click();
+        stateAndCityInput.$(byText("Select City")).click();
+        stateAndCityInput.$(byText(value2)).click();
+
+
+        return this;
+
+    }
+
+    public RegistrationPage clickSubmit() {
+        submitButton.click();
+
+        return this;
+    }
+    public RegistrationPage setWrongUserEmail(String value) {
+        userEmailInput.setValue(value);
+        return this;
+
+    }
+
+
+    public RegistrationPage checkResult(String key, String value) {
+        tableComponent.table(key, value);
+
+        return this;
+
+    }
+
+
+    public RegistrationPage checkNegativeResult() {
+        $(".table-responsive").shouldNotBe(visible);
+
+        return this;
+
+    }
+
+
+}
---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,67 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+public class TextBoxTests extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    void fillFormTest() {
+        registrationPage.openPage()
+                .setFirstName("Simon")
+                .setLastName("Job")
+                .setUserEmail("simon@gmail.com")
+                .setGender("Male")
+                .setUserNumber("8005553535")
+                .setDateOfBirth("31", "October", "1947")
+                .setSubjects("Maths")
+                .setHobbies("Sports", "Reading")
+                .uploadPicture("testPhoto.png")
+                .setCurrentAddress("Moscow street 1")
+                .setStateAndCity("NCR", "Delhi")
+                .clickSubmit()
+                .checkResult("Student Name", "Simon Job")
+                .checkResult("Student Email", "simon@gmail.com")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "8005553535")
+                .checkResult("Date of Birth", "31 October,1947")
+                .checkResult("Subjects", "Maths")
+                .checkResult("Hobbies", "Sports, Reading")
+                .checkResult("Picture", "testPhoto.png")
+                .checkResult("Address", "Moscow street 1")
+                .checkResult("State and City", "NCR Delhi");
+    }
+
+
+    @Test
+    void fillNotFullFormTest() {
+        registrationPage.openPage()
+                .setFirstName("Simon")
+                .setLastName("Job")
+                .setGender("Male")
+                .setUserNumber("8005553535")
+                .setDateOfBirth("31", "October", "1947")
+                .clickSubmit()
+                .checkResult("Student Name", "Simon Job")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "8005553535")
+                .checkResult("Date of Birth", "31 October,1947");
+    }
+
+    @Test
+    void negativeFormTest() {
+
+
+        registrationPage.openPage()
+                .setFirstName("Simon")
+                .setLastName("Job")
+                .setWrongUserEmail("simon@gmail")
+                .setGender("Male")
+                .setUserNumber("8005553535")
+                .setDateOfBirth("31", "October", "1947")
+                .clickSubmit()
+                .checkNegativeResult();
+    }
+}
