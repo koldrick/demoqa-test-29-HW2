7f0c92dc2d222cb917cb08eff4e335cbd720db6f
Simon Kulakov <starckn1@gmail.com>
2024-08-07 17:20:54 +0300

    added remote

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
+import static io.qameta.allure.Allure.step;
+
 
 @Tag("regression")
 public class TextBoxTests extends TestBase {
@@ -15,63 +17,84 @@ public class TextBoxTests extends TestBase {
 
     @Test
     void fillFormTest() {
-
-        registrationPage.openPage()
-                .removeBanner()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setUserEmail(testData.email)
-                .setGender(testData.gender)
-                .setUserNumber(testData.userNumber)
-                .setDateOfBirth(testData.dayOfBDay, testData.monthOfBDay, testData.yearOfBDay)
-                .setSubjects(testData.subject)
-                .setHobbies(testData.hobby)
-                .uploadPicture(testData.picturePath)
-                .setCurrentAddress(testData.address)
-                .setStateAndCity(testData.state, testData.city)
-                .clickSubmit()
-                .checkResult("Student Name", testData.firstName + " " + testData.lastName)
-                .checkResult("Student Email", testData.email)
-                .checkResult("Gender", testData.gender)
-                .checkResult("Mobile", testData.userNumber)
-                .checkResult("Date of Birth", testData.dayOfBDay + " " + testData.monthOfBDay + "," + testData.yearOfBDay)
-                .checkResult("Subjects", testData.subject)
-                .checkResult("Hobbies", testData.hobby)
-                .checkResult("Picture", testData.picturePath)
-                .checkResult("Address", testData.address)
-                .checkResult("State and City", testData.state + " " + testData.city);
+        step("Open form", () -> {
+            registrationPage.openPage()
+                    .removeBanner();
+        });
+        step("Fill form", () -> {
+            registrationPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setUserEmail(testData.email)
+                    .setGender(testData.gender)
+                    .setUserNumber(testData.userNumber)
+                    .setDateOfBirth(testData.dayOfBDay, testData.monthOfBDay, testData.yearOfBDay)
+                    .setSubjects(testData.subject)
+                    .setHobbies(testData.hobby)
+                    .uploadPicture(testData.picturePath)
+                    .setCurrentAddress(testData.address)
+                    .setStateAndCity(testData.state, testData.city);
+        });
+        step("Send form", () -> {
+            registrationPage.clickSubmit();
+        });
+        step("Check result", () -> {
+            registrationPage.checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkResult("Student Email", testData.email)
+                    .checkResult("Gender", testData.gender)
+                    .checkResult("Mobile", testData.userNumber)
+                    .checkResult("Date of Birth", testData.dayOfBDay + " " + testData.monthOfBDay + "," + testData.yearOfBDay)
+                    .checkResult("Subjects", testData.subject)
+                    .checkResult("Hobbies", testData.hobby)
+                    .checkResult("Picture", testData.picturePath)
+                    .checkResult("Address", testData.address)
+                    .checkResult("State and City", testData.state + " " + testData.city);
+        });
     }
 
 
     @Test
     void fillNotFullFormTest() {
-        registrationPage.openPage()
-                .removeBanner()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setGender(testData.gender)
-                .setUserNumber(testData.userNumber)
-                .setDateOfBirth(testData.dayOfBDay, testData.monthOfBDay, testData.yearOfBDay)
-                .clickSubmit()
-                .checkResult("Student Name", testData.firstName + " " + testData.lastName)
-                .checkResult("Gender", testData.gender)
-                .checkResult("Mobile", testData.userNumber)
-                .checkResult("Date of Birth", testData.dayOfBDay + " " + testData.monthOfBDay + "," + testData.yearOfBDay);
+        step("Open form", () -> {
+            registrationPage.openPage()
+                    .removeBanner();
+        });
+        step("Fill form not full", () -> {
+            registrationPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setGender(testData.gender)
+                    .setUserNumber(testData.userNumber)
+                    .setDateOfBirth(testData.dayOfBDay, testData.monthOfBDay, testData.yearOfBDay);
+        });
+        step("Send form", () -> {
+            registrationPage.clickSubmit();
+        });
+        step("Check result", () -> {
+            registrationPage.checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkResult("Gender", testData.gender)
+                    .checkResult("Mobile", testData.userNumber)
+                    .checkResult("Date of Birth", testData.dayOfBDay + " " + testData.monthOfBDay + "," + testData.yearOfBDay);
+        });
     }
 
     @Test
     void negativeFormTest() {
-
-
-        registrationPage.openPage()
-                .removeBanner()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setWrongUserEmail(testData.wrongEmail)
-                .setGender(testData.gender)
-                .setUserNumber(testData.userNumber)
-                .setDateOfBirth(testData.dayOfBDay, testData.monthOfBDay, testData.yearOfBDay)
-                .clickSubmit()
-                .checkNegativeResult();
+        step("Open form", () -> {
+            registrationPage.openPage()
+                    .removeBanner();
+        });
+        step("Fill form not full", () -> {
+            registrationPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setWrongUserEmail(testData.wrongEmail)
+                    .setGender(testData.gender)
+                    .setUserNumber(testData.userNumber)
+                    .setDateOfBirth(testData.dayOfBDay, testData.monthOfBDay, testData.yearOfBDay);
+        });
+        step("Send form", () -> {
+            registrationPage.clickSubmit();
+        });
+        step("Check wrong result", () -> {
+            registrationPage.checkNegativeResult();
+        });
     }
 }
