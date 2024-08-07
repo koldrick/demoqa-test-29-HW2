50aa9d22c0a306fcd7eb5141d5b59fb52ac7c96e
Simon Kulakov <starckn1@gmail.com>
2024-08-07 16:22:27 +0300

    added task for test regression added tags for tests

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,8 +1,11 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
+
+@Tag("regression")
 public class TextBoxTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
