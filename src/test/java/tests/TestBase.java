2aecda88389ca442a1591c498c73d710ebd718ca
Simon Kulakov <starckn1@gmail.com>
2024-08-08 12:03:51 +0300

    delete attach screenshot

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,23 +2,19 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
+
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
 
     @BeforeAll
-    static void beforeAll(){
+    static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl= "https://demoqa.com";
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-    }
+
 }
