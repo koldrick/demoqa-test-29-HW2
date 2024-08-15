aa5a1257152a23eded1594e585f15d23e066dbb3
Simon Kulakov <starckn1@gmail.com>
2024-08-15 10:14:04 +0300

    added smoke_test task

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,12 +15,13 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browserSize = System.getProperty("browserSize","1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion","100.0");
+        Configuration.remote = System.getProperty("remote","https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
-
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
